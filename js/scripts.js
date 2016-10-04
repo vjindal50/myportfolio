55e6b49a6346edbdecf0c25708c1c954e93ce96d
vjindal50 <vjindal@stevens.edu>
2016-10-04 11:17:11 -0400

    major work additions

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,3 @@ $(function () {
   });
 });
 
-var pic = document.getElementbyId('mypic')
-function(){
-    if (screenWidth < 768) {
-      pic.style.height = '80%';
-      pic.style.width = '80%';
-
-    }
-  }
-
-
-//use media query and grid format for web pages
-//to avoid using margin and padding every time
-// and size issue.
